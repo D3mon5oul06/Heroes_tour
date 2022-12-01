cc1df1c783ca3a53d61b9114705d0d621340db9c
D3mon5oul06 <98986072+D3mon5oul06@users.noreply.github.com>
2022-12-01 05:05:36 -0600

    Creacion features #7

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,7 +8,7 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 
-export class HeroesComponent implements OnInit {
+export class heroesComponent implements OnInit {
 
   heroes = HEROES;
   selectedHero?: Hero;
